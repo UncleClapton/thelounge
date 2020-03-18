ec36ff71cbb8e249ac9522e360713ad8483e169e
Cameron Welter <camjwelt@gmail.com>
2020-03-18 09:42:59 +0000

    change coloredNicks to enum, add nickColorRules

--- client/js/settings.js
+++ client/js/settings.js
@@ -27,7 +27,10 @@ export const config = normalizeConfig({
 		default: "",
 	},
 	coloredNicks: {
-		default: true,
+		default: "hash",
+	},
+	nickColorRules: {
+		default: [],
 	},
 	desktopNotifications: {
 		default: false,
