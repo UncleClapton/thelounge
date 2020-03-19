0b449baa91241b754d95f66da8f4cb2efb207e32
Cameron Welter <camjwelt@gmail.com>
2020-03-19 20:38:36 +0000

    Add component for picking nick colors

---
+++ client/components/NickColorPicker.vue
@@ -0,0 +1,20 @@
+<template>
+	<select
+		:class="['nick-color-picker', 'input', 'user', `color-${value}`]"
+		:value="value"
+		v-bind="$attrs"
+	>
+		<option v-for="color in 32" :key="color" :class="`user color-${color}`" :value="color">
+			color-{{ color }}
+		</option>
+	</select>
+</template>
+
+<script>
+export default {
+	name: "NickColorPicker",
+	props: {
+		value: String,
+	},
+};
+</script>
